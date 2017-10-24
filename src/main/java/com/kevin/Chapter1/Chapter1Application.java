361b44b61988b91281fa62eeb8ff78a87dd93e4c
wangshuyu <363852386@qq.com>
2017-10-24 13:15:11 +0800

    Initial commit

---
+++ src/main/java/com/kevin/Chapter1/Chapter1Application.java
@@ -0,0 +1,15 @@
+package com.kevin.Chapter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 通过http://start.spring.io/构建项目
+ */
+@SpringBootApplication
+public class Chapter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter1Application.class, args);
+	}
+}
